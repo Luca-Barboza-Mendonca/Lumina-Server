c7f2bc850ad7028805be33b7218549650413a45a
Luca-Barboza-Mendonca <lucabarbozamendonca@gmail.com>
2023-11-27 21:33:21 -0300

    Chat now performs way better and is TCP again

--- servermanager.py
+++ servermanager.py
@@ -5,7 +5,8 @@ import struct
 import select 
 import util
 
-chatLog = []
+chat_content = util.LimitedQueue(max_size=45)
+chat_client_adresses = []
 
 def find_open_port(host, start_port, end_port):
     open_ports = []
@@ -37,41 +38,40 @@ def receive_message(sock):
     message = sock.recv(message_length).decode()
     return message
 
-def send_messageUDP(sock, addr, message):
-    message_length = len(message)
-    header = struct.pack('!I', message_length)
-    sock.sendto(header + message.encode(), addr)
-
-def receive_messageUDP(sock):
-    data, addr = sock.recvfrom(4096)  # Assuming a maximum message size of 4096 bytes
-    if not data:
-        return None
-    header = data[:4]
-    message_length = struct.unpack('!I', header)[0]
-    message = data[4:4+message_length].decode()
-    return message, addr
-
 
 def chat_socket(port):
-    chat_content = util.LimitedQueue(max_size=45)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(("127.0.0.1", port))
+    sock.listen(12)
     print(f"ChatRoom open on 127.0.0.1 port: {port}")
+    client_sockets = [sock]
 
     while True:
-        
-        chatMessageFromClient, client_address = receive_messageUDP(sock)
-        
-        if chatMessageFromClient == "disconnect":
-            print(f"ChatRoom Closing connection with {client_address}")
+        try:
+            readable, _, _ = select.select(client_sockets, [], [])
+
+            for ready_socket in readable:
+                if ready_socket == sock:
+                    client_socket, client_address = sock.accept()
+                    client_sockets.append(client_socket)
+                    chat_client_adresses.append(client_address)
+                else:
+                    chatMessageFromClient = receive_message(ready_socket)
+
+                    if chatMessageFromClient == None:
+                        print("Closing connection with peer")
+                        ready_socket.close()
+
+                    if chatMessageFromClient in chat_content.queue:
+                        continue
+                    
+                    if chatMessageFromClient != "" and chatMessageFromClient is not None:
+                        print(f"New chat message: {chatMessageFromClient}")
+                        chat_content.enqueue(chatMessageFromClient)
+                    message = "\n".join(map(str, list(chat_content.queue)))
+                    ready_socket.sendall(message.encode("utf-8"))
+        except ValueError:
             continue
-        
-        if chatMessageFromClient != "" and chatMessageFromClient is not None:
-            chat_content.enqueue(chatMessageFromClient)
-        
-        chatString = "\n".join(map(str, list(chat_content.queue)))
-        print(f"Current Chat Log: {chatString}")
-        send_messageUDP(sock, client_address, chatString)
 
 def run_server(port, chatPort):
     server_threads = []
@@ -81,6 +81,7 @@ def run_server(port, chatPort):
 
     chat_thread = threading.Thread(target=chat_socket, args=(chatPort, ))
     chat_thread.start()
+
     
     client_sockets = [server_socket]
     print(f"Waiting for a connection on port {port}...")
